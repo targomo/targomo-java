8ee04ed252dd3e1487fe3b21c0207800bc7d641e
David Redlich <redlich@motionintelligence.net>
2017-07-17 11:54:54 +0200

    Initial commit

---
+++ src/main/java/net/motionintelligence/client/api/request/GeocodingRequest.java
@@ -0,0 +1,15 @@
+package net.motionintelligence.client.api.request;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.HttpMethod;
+
+/**
+ * Calculates coordinates using the ...
+ * TODO proper documentation
+ */
+public class GeocodingRequest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GeocodingRequest.class);
+}
