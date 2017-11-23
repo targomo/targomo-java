3e152d84ce148bc576a574c547f908d951c3f592
David <david.redlich@targomo.com>
2017-11-23 17:30:25 +0100

    replace tabe by 4 spaces in the request classes

--- src/test/java/net/motionintelligence/client/api/request/RouteRequestTest.java
+++ src/test/java/net/motionintelligence/client/api/request/RouteRequestTest.java
@@ -21,62 +21,62 @@ import static org.mockito.Mockito.when;
 
 public class RouteRequestTest extends RequestTest {
 
-	@Test
-	public void get_success() throws Exception {
-		// Mock success response
-		when(sampleResponse.getStatus()).thenReturn(Response.Status.OK.getStatusCode());
+    @Test
+    public void get_success() throws Exception {
+        // Mock success response
+        when(sampleResponse.getStatus()).thenReturn(Response.Status.OK.getStatusCode());
 
-		// Get sample json when success response is queried
-		InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("data/RouteResponse.json");
-		String sampleJson = IOUtils.toString(resourceAsStream, Charset.forName("UTF-8"));
-		when(sampleResponse.readEntity(String.class)).thenReturn(sampleJson);
+        // Get sample json when success response is queried
+        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("data/RouteResponse.json");
+        String sampleJson = IOUtils.toString(resourceAsStream, Charset.forName("UTF-8"));
+        when(sampleResponse.readEntity(String.class)).thenReturn(sampleJson);
 
-		// Make the call
-		RouteRequest routeRequest = new RouteRequest(mockClient, getTravelOptions());
-		RouteResponse routeResponse = routeRequest.get();
+        // Make the call
+        RouteRequest routeRequest = new RouteRequest(mockClient, getTravelOptions());
+        RouteResponse routeResponse = routeRequest.get();
 
-		// Check result
-		assertEquals("ok", routeResponse.getCode());
-		assertEquals(39, routeResponse.getRequestTimeMillis());
-		assertEquals(1, routeResponse.getRoutes().length());
+        // Check result
+        assertEquals("ok", routeResponse.getCode());
+        assertEquals(39, routeResponse.getRequestTimeMillis());
+        assertEquals(1, routeResponse.getRoutes().length());
 
-		JSONObject route = (JSONObject) routeResponse.getRoutes().get(0);
-		assertEquals(483, route.getInt("travelTime"));
-		assertEquals(4064.2324777363283, route.getDouble("length"), 0);
-		assertEquals("target0", route.getString("target_id"));
-		assertEquals("source0", route.getString("source_id"));
-		assertNotNull(route.getJSONArray("segments"));
-	}
+        JSONObject route = (JSONObject) routeResponse.getRoutes().get(0);
+        assertEquals(483, route.getInt("travelTime"));
+        assertEquals(4064.2324777363283, route.getDouble("length"), 0);
+        assertEquals("target0", route.getString("target_id"));
+        assertEquals("source0", route.getString("source_id"));
+        assertNotNull(route.getJSONArray("segments"));
+    }
 
-	@Test
-	public void get_gateway_timeout() throws Exception {
-		when(sampleResponse.getStatus()).thenReturn(Response.Status.GATEWAY_TIMEOUT.getStatusCode());
+    @Test
+    public void get_gateway_timeout() throws Exception {
+        when(sampleResponse.getStatus()).thenReturn(Response.Status.GATEWAY_TIMEOUT.getStatusCode());
 
-		TravelOptions options = getTravelOptions();
-		RouteRequest routeRequest = new RouteRequest(mockClient, options);
-		RouteResponse routeResponse = routeRequest.get();
-		assertEquals("gateway-time-out", routeResponse.getCode());
-	}
+        TravelOptions options = getTravelOptions();
+        RouteRequest routeRequest = new RouteRequest(mockClient, options);
+        RouteResponse routeResponse = routeRequest.get();
+        assertEquals("gateway-time-out", routeResponse.getCode());
+    }
 
-	@Test(expected = Route360ClientException.class)
-	public void get_exception() throws Exception {
-		when(sampleResponse.getStatus()).thenReturn(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    @Test(expected = Route360ClientException.class)
+    public void get_exception() throws Exception {
+        when(sampleResponse.getStatus()).thenReturn(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
 
-		TravelOptions options = getTravelOptions();
-		RouteRequest routeRequest = new RouteRequest(mockClient, options);
-		routeRequest.get();
-	}
+        TravelOptions options = getTravelOptions();
+        RouteRequest routeRequest = new RouteRequest(mockClient, options);
+        routeRequest.get();
+    }
 
-	private TravelOptions getTravelOptions() {
-		TravelOptions options = new TravelOptions();
-		options.addSource(new DefaultSourceCoordinate("source0", 13.386154, 52.516221, TravelType.CAR));
-		options.addTarget(new DefaultTargetCoordinate("target0", 13.41259, 52.497518));
-		options.setPathSerializer(PathSerializerType.COMPACT_PATH_SERIALIZER);
-		options.setElevationEnabled(true);
-		options.setReverse(false);
+    private TravelOptions getTravelOptions() {
+        TravelOptions options = new TravelOptions();
+        options.addSource(new DefaultSourceCoordinate("source0", 13.386154, 52.516221, TravelType.CAR));
+        options.addTarget(new DefaultTargetCoordinate("target0", 13.41259, 52.497518));
+        options.setPathSerializer(PathSerializerType.COMPACT_PATH_SERIALIZER);
+        options.setElevationEnabled(true);
+        options.setReverse(false);
 
-		options.setServiceKey("INSERT_YOUR_KEY_HERE");
-		options.setServiceUrl("https://service.route360.net/na_northeast/");
-		return options;
-	}
+        options.setServiceKey("INSERT_YOUR_KEY_HERE");
+        options.setServiceUrl("https://service.route360.net/na_northeast/");
+        return options;
+    }
 }
--- src/test/java/net/motionintelligence/client/api/request/TimeRequestTest.java
+++ src/test/java/net/motionintelligence/client/api/request/TimeRequestTest.java
@@ -20,57 +20,57 @@ import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 
 public class TimeRequestTest extends RequestTest {
-	@Test
-	public void get_success() throws Exception {
-		// Mock success response
-		when(sampleResponse.getStatus()).thenReturn(Response.Status.OK.getStatusCode());
+    @Test
+    public void get_success() throws Exception {
+        // Mock success response
+        when(sampleResponse.getStatus()).thenReturn(Response.Status.OK.getStatusCode());
 
-		// Get sample json when success response is queried
-		InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("data/TimeResponse.json");
-		String sampleJson = IOUtils.toString(resourceAsStream, Charset.forName("UTF-8"));
-		when(sampleResponse.readEntity(String.class)).thenReturn(sampleJson);
+        // Get sample json when success response is queried
+        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("data/TimeResponse.json");
+        String sampleJson = IOUtils.toString(resourceAsStream, Charset.forName("UTF-8"));
+        when(sampleResponse.readEntity(String.class)).thenReturn(sampleJson);
 
-		TimeRequest timeRequest = new TimeRequest(mockClient, getTravelOptions());
-		TimeResponse timeResponse = timeRequest.get();
+        TimeRequest timeRequest = new TimeRequest(mockClient, getTravelOptions());
+        TimeResponse timeResponse = timeRequest.get();
 
-		assertEquals("ok", timeResponse.getCode());
-		assertEquals(472, timeResponse.getRequestTimeMillis());
-		assertNotNull(timeResponse.getTravelTimes());
-	}
+        assertEquals("ok", timeResponse.getCode());
+        assertEquals(472, timeResponse.getRequestTimeMillis());
+        assertNotNull(timeResponse.getTravelTimes());
+    }
 
-	@Test
-	public void get_gateway_timeout() throws Exception {
-		when(sampleResponse.getStatus()).thenReturn(Response.Status.GATEWAY_TIMEOUT.getStatusCode());
+    @Test
+    public void get_gateway_timeout() throws Exception {
+        when(sampleResponse.getStatus()).thenReturn(Response.Status.GATEWAY_TIMEOUT.getStatusCode());
 
-		TravelOptions options = getTravelOptions();
-		TimeRequest timeRequest = new TimeRequest(mockClient, options);
-		TimeResponse timeResponse = timeRequest.get();
-		assertEquals("gateway-time-out", timeResponse.getCode());
-	}
+        TravelOptions options = getTravelOptions();
+        TimeRequest timeRequest = new TimeRequest(mockClient, options);
+        TimeResponse timeResponse = timeRequest.get();
+        assertEquals("gateway-time-out", timeResponse.getCode());
+    }
 
-	@Test(expected = Route360ClientException.class)
-	public void get_exception() throws Exception {
-		when(sampleResponse.getStatus()).thenReturn(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    @Test(expected = Route360ClientException.class)
+    public void get_exception() throws Exception {
+        when(sampleResponse.getStatus()).thenReturn(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
 
-		TravelOptions options = getTravelOptions();
-		TimeRequest timeRequest = new TimeRequest(mockClient, options);
-		timeRequest.get();
-	}
+        TravelOptions options = getTravelOptions();
+        TimeRequest timeRequest = new TimeRequest(mockClient, options);
+        timeRequest.get();
+    }
 
-	private TravelOptions getTravelOptions() {
-		TravelOptions options = new TravelOptions();
-		options.setTravelTimes(Arrays.asList(600, 1200, 1800));
-		options.setTravelType(TravelType.CAR);
-		options.setPathSerializer(PathSerializerType.COMPACT_PATH_SERIALIZER);
-		options.addSource(new DefaultSourceCoordinate("id0", 10.639872441947901, -17.37236573607632, TravelType.CAR));
-		options.addSource(new DefaultSourceCoordinate("id1", 49.75913939827413, -75.83100508496594, TravelType.CAR));
-		options.addSource(new DefaultSourceCoordinate("id2", 64.68336864385466, 62.545199028051314, TravelType.CAR));
-		options.addTarget(new DefaultTargetCoordinate("id0", -84.01440151465849, 41.805968836500426));
-		options.addTarget(new DefaultTargetCoordinate("id1", 71.34898703519798, -36.16051650737182));
-		options.addTarget(new DefaultTargetCoordinate("id2", 57.12383436652155, 33.64895304828113));
-		options.setServiceKey("INSERT_YOUR_KEY_HERE");
-		options.setServiceUrl("https://service.route360.net/na_northeast/");
-		return options;
-	}
+    private TravelOptions getTravelOptions() {
+        TravelOptions options = new TravelOptions();
+        options.setTravelTimes(Arrays.asList(600, 1200, 1800));
+        options.setTravelType(TravelType.CAR);
+        options.setPathSerializer(PathSerializerType.COMPACT_PATH_SERIALIZER);
+        options.addSource(new DefaultSourceCoordinate("id0", 10.639872441947901, -17.37236573607632, TravelType.CAR));
+        options.addSource(new DefaultSourceCoordinate("id1", 49.75913939827413, -75.83100508496594, TravelType.CAR));
+        options.addSource(new DefaultSourceCoordinate("id2", 64.68336864385466, 62.545199028051314, TravelType.CAR));
+        options.addTarget(new DefaultTargetCoordinate("id0", -84.01440151465849, 41.805968836500426));
+        options.addTarget(new DefaultTargetCoordinate("id1", 71.34898703519798, -36.16051650737182));
+        options.addTarget(new DefaultTargetCoordinate("id2", 57.12383436652155, 33.64895304828113));
+        options.setServiceKey("INSERT_YOUR_KEY_HERE");
+        options.setServiceUrl("https://service.route360.net/na_northeast/");
+        return options;
+    }
 
 }
