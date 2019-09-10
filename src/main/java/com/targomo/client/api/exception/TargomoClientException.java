2bc04d103efb79432ad8ded04a8ad6fce8d77916
gideon <gideon.cohen@targomo.com>
2019-09-10 16:06:11 +0200

    Revert "Add httpStatus to TargomoClientException"
    This reverts commit 23bdff24c6d8aba3821cea0969a199b6b981c77e.

--- src/main/java/com/targomo/client/api/exception/TargomoClientException.java
+++ src/main/java/com/targomo/client/api/exception/TargomoClientException.java
@@ -3,37 +3,12 @@ package com.targomo.client.api.exception;
 public class TargomoClientException extends Exception {
 
 	private static final long serialVersionUID = 748077356312331362L;
-	private int httpStatus;
 
 	public TargomoClientException(String message, Throwable throwable) {
 		super(message, throwable);
-		httpStatus = 0;
 	}
 
 	public TargomoClientException(String message) {
 		super(message);
-		httpStatus = 0;
-	}
-
-	public TargomoClientException(String message, Throwable throwable, int httpStatus) {
-		super(message, throwable);
-		this.httpStatus = httpStatus;
-	}
-
-	public TargomoClientException(String message, int httpStatus) {
-		super(message);
-		this.httpStatus = httpStatus;
-	}
-
-	public boolean hasHttpStatus() {
-		return httpStatus != 0;
-	}
-
-	public int getHttpStatus() {
-		return httpStatus;
-	}
-
-	public void setHttpStatus(int status) {
-		httpStatus = status;
 	}
 }
