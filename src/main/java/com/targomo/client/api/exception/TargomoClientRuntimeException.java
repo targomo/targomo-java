add7af7c30b5dd90c68a0ec48bd0923c6a6a5c8c
gideon <gideon.cohen@targomo.com>
2019-09-24 10:54:26 +0200

    Make TargomoClientRuntimeException serialVersionUID unique

--- src/main/java/com/targomo/client/api/exception/TargomoClientRuntimeException.java
+++ src/main/java/com/targomo/client/api/exception/TargomoClientRuntimeException.java
@@ -2,7 +2,7 @@ package com.targomo.client.api.exception;
 
 public class TargomoClientRuntimeException extends RuntimeException {
 
-	private static final long serialVersionUID = 748077356312331362L;
+	private static final long serialVersionUID = -7860204374116258864L;
 
 	public TargomoClientRuntimeException(String message, Throwable throwable) {
 		super(message, throwable);
