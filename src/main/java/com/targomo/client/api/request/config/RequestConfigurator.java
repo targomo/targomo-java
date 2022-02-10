202e2e34142196828f1a0ab0b3714c4c9f4e95a3
Hugo <hugo.lucas@targomo.com>
2022-02-10 15:47:56 +0100

    no need for interservice key in the payload

--- src/main/java/com/targomo/client/api/request/config/RequestConfigurator.java
+++ src/main/java/com/targomo/client/api/request/config/RequestConfigurator.java
@@ -131,9 +131,6 @@ public final class RequestConfigurator {
             if (travelOptions.getServiceKey() != null)
                 JSONBuilder.append(config, "serviceKey", "\"" + travelOptions.getServiceKey() + "\"");
 
-            if (travelOptions.getInterServiceKey() != null)
-                JSONBuilder.appendString(config, Constants.INTER_SERVICE_KEY, travelOptions.getInterServiceKey());
-
             if (travelOptions.getFormat() != null)
                 JSONBuilder.append(config, Constants.FORMAT, "\"" + travelOptions.getFormat().toString().toLowerCase() + "\"");
 
