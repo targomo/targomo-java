f77edaec1bcb15ae72cc480ec35aab23e8f3d243
Mohit Gupta <mohit@Targomo-A0065.local>
2022-08-25 12:57:06 +0200

    Adding delta changes in Criterion Definitions

--- src/main/java/com/targomo/client/api/quality/criterion/StatisticsGravitationCriterionDefinition.java
+++ src/main/java/com/targomo/client/api/quality/criterion/StatisticsGravitationCriterionDefinition.java
@@ -1,5 +1,6 @@
 package com.targomo.client.api.quality.criterion;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import com.targomo.client.api.quality.Location;
@@ -16,6 +17,7 @@ import java.util.List;
 @SuperBuilder(toBuilder = true)
 @ToString(callSuper = true)
 @JsonDeserialize(builder = StatisticsGravitationCriterionDefinition.StatisticsGravitationCriterionDefinitionBuilderImpl.class)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class StatisticsGravitationCriterionDefinition extends StatisticsReachabilityCriterionDefinition implements GravitationCriterionDefinition {
 
     @Setter
