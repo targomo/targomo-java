93fa4b15842e330a30a545fecc0b7e201d50b5ac
Hugo <hugo.lucas@targomo.com>
2022-09-23 10:39:02 +0200

    fix gravitation criterion interface

--- src/main/java/com/targomo/client/api/quality/criterion/GravitationCriterionDefinition.java
+++ src/main/java/com/targomo/client/api/quality/criterion/GravitationCriterionDefinition.java
@@ -1,10 +1,15 @@
 package com.targomo.client.api.quality.criterion;
 
+import com.targomo.client.api.enums.TravelType;
 import com.targomo.client.api.quality.Location;
 
 import java.util.List;
 
 public interface GravitationCriterionDefinition {
+
+    TravelType getTravelType();
+
+    Integer getMaxEdgeWeight();
 
     List<Location> getCompetitors();
 
