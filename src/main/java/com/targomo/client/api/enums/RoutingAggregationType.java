6cb8376474ed333de28c4b0db88eded95b4afccb
Mohit Gupta <mohit@Targomo-A0065.local>
2022-11-22 05:55:15 +0100

    Overriding hashcode because of enum in CompetingRouting

--- src/main/java/com/targomo/client/api/enums/RoutingAggregationType.java
+++ src/main/java/com/targomo/client/api/enums/RoutingAggregationType.java
@@ -1,7 +1,9 @@
 package com.targomo.client.api.enums;
 
+import java.util.Objects;
+
 public enum RoutingAggregationType {
     MIN,
     MEAN,
-    SUM
+    SUM;
 }
--- src/main/java/com/targomo/client/api/pojo/CompetingRoutingOption.java
+++ src/main/java/com/targomo/client/api/pojo/CompetingRoutingOption.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.targomo.client.api.enums.RoutingAggregationType;
 import lombok.*;
 
+import java.util.Objects;
+
 @ToString
 @EqualsAndHashCode
 public class CompetingRoutingOption {
@@ -12,7 +14,7 @@ public class CompetingRoutingOption {
     public final RoutingOptions routingOptions;
     public final int routingValueOffset;
     public final float routingValueMultiplier;
-    @EqualsAndHashCode.Exclude public final RoutingAggregationType routingAggregationType;
+    public final RoutingAggregationType routingAggregationType;
 
     @Builder
     @JsonCreator
@@ -24,5 +26,10 @@ public class CompetingRoutingOption {
         this.routingValueOffset = routingValueOffset == null ? 0 : routingValueOffset;
         this.routingValueMultiplier = routingValueMultiplier == null ? 1.0f : routingValueMultiplier;
         this.routingAggregationType = routingAggregationType == null ? RoutingAggregationType.MIN :routingAggregationType;
+    }
+
+    @Override //overriding hash code since the has would change with each execution for enums (routingAggregationType)
+    public int hashCode(){
+        return Objects.hash(routingOptions, routingValueOffset, routingValueMultiplier, routingAggregationType.toString());
     }
 }
