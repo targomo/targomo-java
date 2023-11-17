15557b8999d1fd76ab246aa981d27b149081fa4a
Hugo <hugo.lucas@targomo.com>
2023-11-17 18:30:22 +0100

    fix serialization

--- src/main/java/com/targomo/client/api/quality/criterion/CriterionMetadata.java
+++ src/main/java/com/targomo/client/api/quality/criterion/CriterionMetadata.java
@@ -4,9 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 
 @Getter
 @AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@Jacksonized
 @EqualsAndHashCode
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CriterionMetadata {
