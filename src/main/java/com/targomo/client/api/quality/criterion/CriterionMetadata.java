603d44b2b754d06c4437b8606c5a2f816ac87170
Hugo <hugo.lucas@targomo.com>
2023-11-17 15:52:15 +0100

    add criterion metadata

---
+++ src/main/java/com/targomo/client/api/quality/criterion/CriterionMetadata.java
@@ -0,0 +1,12 @@
+package com.targomo.client.api.quality.criterion;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class CriterionMetadata {
+    private final String name;
+}
