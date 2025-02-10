366747ebf17ccd25b3c5b175ae6a158bb91640e4
juri.engel <juri.engel@targomo.com>
2025-02-10 18:19:04 +0100

    Add parameters to aggregate edge statistics

--- src/main/java/com/targomo/client/api/pojo/EdgeStatisticsRequestOptions.java
+++ src/main/java/com/targomo/client/api/pojo/EdgeStatisticsRequestOptions.java
@@ -1,6 +1,7 @@
 package com.targomo.client.api.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.targomo.client.api.enums.EdgeStatisticAggregationType;
 import com.targomo.client.api.enums.EdgeStatisticDirection;
 import com.targomo.client.api.enums.TravelType;
 import com.targomo.client.api.geo.DefaultTargetCoordinate;
@@ -10,7 +11,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Parameters for an edge statistics location request.
@@ -20,6 +23,10 @@ import java.util.List;
 public class EdgeStatisticsRequestOptions {
 
     private List<Integer> edgeStatisticIds = new ArrayList<>();
+
+    private Map<String, List<Integer>> aggregateEdgeStatisticIds = new HashMap<>();
+
+    private EdgeStatisticAggregationType aggregationType = EdgeStatisticAggregationType.SUM;
 
     private Integer radius = 100;
     private TravelType travelType = TravelType.CAR;
