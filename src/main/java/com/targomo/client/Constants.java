b675fd5aa4d880fa8dc67d04a4316cbdbb7cc653
Hugo Lucas <hugo.lucas@targomo.com>
2025-12-07 01:24:07 +0100

    fix spaces

--- src/main/java/com/targomo/client/Constants.java
+++ src/main/java/com/targomo/client/Constants.java
@@ -93,7 +93,7 @@ public class Constants {
     public static final String USE_AREA_SNAPPING                                        = "useAreaSnapping";
     public static final String SNAP_RADIUS                                              = "snapRadius";
     public static final String EXCLUDE_EDGE_CLASSES_FROM_SNAPPING                       = "excludeEdgeClassesFromSnapping";
-    public static final String SNAPPING_SPEED                               = "snappingSpeed";
+    public static final String SNAPPING_SPEED                                           = "snappingSpeed";
 
     public static final String MULTIGRAPH                                               = "multigraph";
     public static final String MULTIGRAPH_EDGE_CLASSES                                  = "edgeClasses";
