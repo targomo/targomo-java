a4b0abf069246823bf473d68f37704d46a472e56
juri.engel <juri.engel@targomo.com>
2026-07-09 17:36:52 +0200

    Add some helper function for refactoring in quality service

--- src/main/java/com/targomo/client/api/quality/criterion/RoutingBasedCriterionDefinition.java
+++ src/main/java/com/targomo/client/api/quality/criterion/RoutingBasedCriterionDefinition.java
@@ -2,6 +2,7 @@ package com.targomo.client.api.quality.criterion;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.targomo.client.api.TravelOptions;
 import com.targomo.client.api.enums.EdgeWeightType;
 import com.targomo.client.api.enums.TravelType;
 import com.targomo.client.api.exception.TargomoClientRequestConfigurationException;
@@ -71,6 +72,34 @@ public abstract class RoutingBasedCriterionDefinition extends CriterionDefinitio
         }
 
         return (Map) propertiesObj;
+    }
+
+    @JsonIgnore
+    public TravelOptions createTravelOptionsWithoutSources() {
+        return populateTravelOptionsWithoutSources(new TravelOptions());
+    }
+
+    @JsonIgnore
+    public TravelOptions populateTravelOptionsWithoutSources(TravelOptions options) {
+        options.setEdgeWeightType(edgeWeight);
+        options.setMaxEdgeWeight(maxEdgeWeight);
+
+        options.setTravelType(this.getTravelType());
+        if (elevation != null) {
+            options.setElevationEnabled(elevation);
+        }
+        if (travelTimeFactors != null) {
+            options.setTravelTimeFactors(travelTimeFactors);
+        }
+        if (defaultRoadSpeeds != null) {
+            options.setDefaultRoadSpeeds(defaultRoadSpeeds);
+        }
+        if(reverse != null){
+            options.setReverse(reverse);
+        }
+        options.setExclusionGeometry(exclusionGeometry);
+        options.setExcludeEdgeClasses(excludeEdgeClasses);
+        return options;
     }
 
 }
